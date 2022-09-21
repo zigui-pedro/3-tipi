b5772fb2d383cd53adf831f9dc124994a27be7bc
Murillo jesus <murillos.jesus@hotmail.com>
2022-09-21 17:43:13 +0000

    Update 9 files

---
+++ src/components/MeuPerfil.vue
@@ -0,0 +1,19 @@
+<template>
+    <h1>
+        Meu perfil
+    </h1>
+</template>
+
+<script  lang=ts>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+    name: "Meu Perfil"      
+})
+</script>
+
+<style scoped>
+    h1 {
+        color: purple;
+    }
+</style> 
